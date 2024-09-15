e50d6eccfd8b621d549e1efb48d2922752aed723
Ale <cruzmaralis17@gmail.com>
2024-09-14 22:50:31 -0400

    Django CRUD - Libros

---
+++ sistema/libreria/urls.py
@@ -0,0 +1,19 @@
+#
+from django.urls import path
+from .import views
+
+#Para ver las imagenes importanto lo que se modifico en settings django
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+
+
+urlpatterns = [
+    #Enlaces 
+    path('',views.inicio, name='inicio'),
+    path('nosotros', views.nosotros, name='nosotros'),
+    path('libros', views.libros, name='libros'),
+    path('libros/crear', views.crear, name='crear'),
+    path('eliminar/<int:id>', views.eliminar, name='eliminar'),
+    path('libros/editar/<int:id>/', views.editar, name='editar'),
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
